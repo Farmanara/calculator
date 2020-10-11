43a479e0b1b2e999670d66e9ae0c99f5ddb2e943
Fighter <farmanara.code@gmail.com>
2020-10-12 00:29:55 +0330

    Updates

---
+++ precache-manifest.783436df13e9c80a0413f0a7411bb3f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4cc6027b6e5f9bbd03fe549babfb6c2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "2c5d4855d85eaaea5b1d",
+    "url": "/calculator/static/css/main.e4c6e72d.chunk.css"
+  },
+  {
+    "revision": "04837a0a1b92c4fb2df1",
+    "url": "/calculator/static/js/2.33f20fc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.33f20fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5d4855d85eaaea5b1d",
+    "url": "/calculator/static/js/main.1e76918d.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
