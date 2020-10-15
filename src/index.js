00dc1bc411df0ff8cb90662b25a37ba764e8eeae
Fighter <farmanara.code@gmail.com>
2020-10-15 23:12:26 +0330

    all user stories met. just needs some styling

--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import '../src/index.css'
 
+
+
+
 class Calculator extends React.Component{
     constructor(props){
 super(props);
 
 this.state= {
-    display:'0',
-    firstInput:'',
-    secondInput:'',
+    display:"0",
+    Input:'0',
+    dotStatus:false,
     operator:'',
     operatorClicked:false,
-    equalsClicked:false
+    //equalsClicked:false
 }
 
 
 this.clear=this.clear.bind(this);
 this.onClick=this.onClick.bind(this);
 this.equals=this.equals.bind(this);
-this.operatorClicked=this.operatorClicked.bind(this);
 
     }
 
@@ -32,12 +34,10 @@ this.operatorClicked=this.operatorClicked.bind(this);
 clear(){
     
 this.setState({
-    display:'0',
-    firstInput:'',
-    secondInput:'',
-    operator:'',
-    operatorClicked:false,
-    equalsClicked:false,
+    display:"0",
+    Input:'0',
+    
+    
     dotStatus:false
 })
 }
@@ -45,78 +45,101 @@ this.setState({
 
 //gets the inputs
     onClick(e){
-     
-console.log(e.target.value)
-if ((this.state.dotStatus==true)&&(e.target.value==".")){
+
+if(e.target.className==="operator"){
+    this.setState({
+        dotStatus:false,
+    },()=>{})
+
+}
+
+if ((this.state.dotStatus===true)&&(e.target.value===".")){
     return
 }
-if(e.target.value==".") {
+
+if(e.target.value===".") {
 this.setState({
     dotStatus:true
-})
-}
- if ((this.state.operatorClicked===false) && (parseFloat(e.target.value)!==0)){
-    this.setState({firstInput:this.state.firstInput+e.target.value,
-                    display:this.state.firstInput+e.target.value},()=>{
-        console.log("after:"+(this.state.firstInput))
-    })}
-    else if(parseFloat(e.target.value)!==0){
+})}
 
-        this.setState({secondInput:this.state.secondInput+e.target.value,
-            display:this.state.secondInput+e.target.value},()=>{
-            console.log("after second:"+(this.state.secondInput))
-        })}
-    }
+
+     if((this.state.Input+e.target.value)!=="00"){
+    this.setState({display:e.target.value})
+
+         let lastInput=this.state.Input;
+       console.log("lastInput:"+lastInput)
+
+//prevent double operators
+let lastLetter=lastInput.charAt(lastInput.length-1)
+ console.log("lastletter:"+lastLetter)
+ let clickedValue= e.target.value;
+ console.log ('clickedItemValue: ' + clickedValue)
+ let secondLastLetter= lastInput.charAt(lastInput.length-2);
+
+ // prevents double negatives 
+if ((lastLetter=="-") && (clickedValue=="-")) {
     
+    let trimmed = lastInput.substring(0, lastInput.length - 1)+ clickedValue;
+   this.setState({
+       Input:trimmed,
+       display:trimmed
+   })
+   return
+}
+// turns *-+ to +
+ if((secondLastLetter=="/"||secondLastLetter=="*"||secondLastLetter=="+")&&(lastLetter=="-") && (clickedValue=="/"||clickedValue=="*"||clickedValue=="+" ) ){
+     let trimmed2 = lastInput.substring(0,lastInput.length-2)+clickedValue;
+     this.setState({
+         Input:trimmed2,
+         display:trimmed2
+     })
+     return
+ }
 
- 
+ // replaces the operator to the newest operator clicked consecutively
+ if((lastLetter=="/"||lastLetter=="*"||lastLetter=="+"||lastLetter=="-") && (clickedValue=="/"||clickedValue=="*"||clickedValue=="+" ) )
+ {
+     console.log("condition test")
+     let trimmed = lastInput.substring(0, lastInput.length - 1)+ clickedValue;
+     console.log("trimmed: " +trimmed)
+    this.setState({
+        Input:trimmed,
+        display:trimmed
+    })
+    return
 
-//gets the operator 
+ }
 
-operatorClicked(e){
-    let z = e.target.value;
 
-this.setState({
-    operatorClicked:true,
-    operator:z,
-},()=>{console.log(z)})}
-
+       if (this.state.Input=="0") {
+            
+          this.setState({
+              Input:e.target.value
+          })
+          return
+       } 
+        let input=lastInput+e.target.value
+        console.log("lastInput+value:"+ input)
+       
+    this.setState({Input:input,
+                    display:input})
+                    
+    }
+}
+                
 
  //shows the result
 
 equals(){
-switch(this.state.operator) {
-    case "+":
-        this.setState({
-            display:parseFloat(this.state.firstInput)+parseFloat(this.state.secondInput)
-        })
-        return
-    case "-":
-        this.setState({
-        display:parseFloat(this.state.firstInput)-parseFloat(this.state.secondInput)
-         })
-         return
-    case "/":
-        this.setState({
-            display:parseFloat(this.state.firstInput)/parseFloat(this.state.secondInput)
-             })
-        return
-    case "*":
-        this.setState({
-            display:parseFloat(this.state.firstInput)*parseFloat(this.state.secondInput)
-         })
-         
-
-
+    let results=this.state.Input
+    let stringResult=eval(results).toString();
     this.setState({
-        firstInput:'',
-        secondInput:'',
-        operator:'',
-        operatorClicked:false,
-        equalsClicked:false
-    })
-}}
+        display:eval(results),
+        Input:stringResult
 
+    })
+    
+}
 
 
 
@@ -137,12 +160,12 @@ switch(this.state.operator) {
                 <button className="item 7" id="seven" value="7" onClick={this.onClick} >7</button>
                 <button className="item 8" id="eight" value="8" onClick={this.onClick}>8</button>
                 <button className="item 9" id="nine" value="9" onClick={this.onClick}  >9</button>
-                <button className="item add" id="add" value="+" onClick={this.operatorClicked} >+</button>
-                <button className="item subtract" value="-" id="subtract" onClick={this.operatorClicked}>-</button>
-                <button className="item multiply" value="*" id="multiply" onClick={this.operatorClicked}>x</button>
-                <button className="item divide" id="divide" value="/" onClick={this.operatorClicked}>/</button>
-                <button className="item decimal" id="decimal" value="." onClick={this.onClick}>.</button>
-                <button className="item clear" id="clear" onClick={this.clear} value="clear" onClick={this.clear}>C</button>
+                <button className="operator" id="add" value="+" onClick={this.onClick} >+</button>
+                <button className="operator" value="-" id="subtract" onClick={this.onClick}>-</button>
+                <button className="operator" value="*" id="multiply" onClick={this.onClick}>x</button>
+                <button className="operator" id="divide" value="/" onClick={this.onClick}>/</button>
+                <button className="item decimal " id="decimal" value="." onClick={this.onClick}>.</button>
+                <button className="item clear" id="clear"  value="clear" onClick={this.clear}>C</button>
                 <button className="item equal" id="equals" value="=" onClick={this.equals}>=</button>
             </div>
             // </div>
